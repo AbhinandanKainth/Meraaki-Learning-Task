5879afd17e45c5fb7b1d3d6af12d6d52a156b1ff
abhinandan kainth <39798134+kainthcool@users.noreply.github.com>
2020-08-27 22:13:36 +0530

    added post req

--- public/index.js
+++ public/index.js
@@ -1,11 +1,17 @@
 var app5 = new Vue({
     el: '#app-5',
     data: {
-      message: '_'
+      message: '_',
+      no: '1'
     },
     methods: {
-      getpost: function () {
-        fetch('https://jsonplaceholder.typicode.com/posts/1')
+      getpost: function (no) {
+        var link;
+        if(no != 'all')
+          link = 'https://jsonplaceholder.typicode.com/posts/' + no;
+        else
+          link = link = 'https://jsonplaceholder.typicode.com/posts';
+        fetch(link)
         .then((response) => {
             return response.json()
         })
@@ -13,6 +19,9 @@ var app5 = new Vue({
             // Work with JSON data here
             // console.log(data);
             this.message = JSON.stringify(data);
+            // document.writeln(JSON.stringify(data, null, '\t'));
+            // window.alert(JSON.stringify(data, null, '\t'));
+            // this.message// console.log(JSON.stringify(JSON.parse(data), null, 2))
             
         })
         .catch((err) => {
@@ -20,4 +29,37 @@ var app5 = new Vue({
         })
       }
     }
+  })
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: '',
+      title_: 'Title',
+      body_ : 'Body',
+      userId_: 'userID',
+    },
+    methods: {
+      getpost: function (title_,body_,userId_) {
+          fetch('https://jsonplaceholder.typicode.com/posts', {
+          method: 'POST',
+          body: JSON.stringify({
+            title: title_,
+            body: body_,
+            userId: userId_
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.message = json;
+          return console.log(json);
+        })
+        
+        }
+    }
+
   })
